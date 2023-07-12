54b1c6a3649e164939340d742b939d4f0e1c2557
ranjan7707 <ranjan7707@gmail.com>
2023-07-12 13:19:37 +0530

    ExpressJS Q17

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'HDFCrr@7707', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
